d44dec24835efa542f2ebff7b59909630f187b5f
Cyrus Singer <japaneserhino@gmail.com>
2022-11-12 15:41:20 -0500

    Update HomePage.js

--- website1/src/Components/HomePage.js
+++ website1/src/Components/HomePage.js
@@ -100,7 +100,8 @@ export default ({ onAdopt, onProfilePicClicked }) => {
           </Col>
           <Col>
             <Image
-              scr="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+              scr="https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg"
+              fluid
             />
           </Col>
         </Row>
